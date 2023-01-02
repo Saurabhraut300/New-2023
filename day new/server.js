d1b7f8a6933e07b9af9f652ea434429c4c829fe3
Saurabh raut <rautsaurabh283@gmail.com>
2023-01-02 17:40:18 +0530

    new

--- day new/server.js
+++ day new/server.js
@@ -1,15 +1,13 @@
-var express = require('express');
-      var app = express();
-      var path=require("path");
-      var fs=require("fs");
-  //server configuration
-  app.use(express.static(path.join(__dirname,'public')));
-  app.get('/', function (req, res) {
-   res.sendFile(path.join(___dirname + '/index.html'));
+const express=require('express');
+const path=require('path');
+const app=express();
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/new.html'));
 });
-  
-  var server = app.listen(8081, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log("Example app listening at http://localhost:8081", host, port)
-})
+app.get('/aboutus',(req,res)=>
+{
+    var person={firstName:'akshay',LastName:'Hambarde',id:10};
+    res.send(person);
+
+});
+app.listen(3001);
